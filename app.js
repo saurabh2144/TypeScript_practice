ab797b566d09b51a21337322bcb0cd9999d973bd
saurabh singh <itsmesaurabh214@gmail.com>
2025-09-06 08:41:45 +0530

    k

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 let a = "saurabh";
+let arr = [22, "sau"];
+let pp = [1];
+let b;
+b = 12;
+b = "sss";
+console.log(b);
+function show(a, b) {
+    console.log(a, b);
+}
+show(3, "sj");
+function join(obj) {
+    if (obj.age > 18) {
+        console.log(obj.age);
+    }
+    console.log('====================================');
+    console.log("number is not  greater than 18 ");
+    console.log('====================================');
+}
+join({ name: "saurabh", age: 14, address: "hayatganj" });
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,2 +1,37 @@
 let a = "saurabh";
 
+
+let arr : [number , string]=[ 22 , "sau"] ;
+let pp : [number] = [1]
+
+let b : number | string ;
+
+b= 12;
+b="sss";
+console.log(b);
+
+function  show (a :number ,b :string) :void {
+    console.log(a,b);
+}
+
+show(3 ,"sj");
+
+interface obj1 {
+    name:string,
+    age: number,
+    address : string,
+    gender ?: string 
+}
+
+
+function join (obj : obj1) : void {
+    if ( obj.age > 18) {
+        console.log(obj.age);
+        
+    }
+    console.log('====================================');
+    console.log("number is not  greater than 18 ");
+    console.log('====================================');
+}
+
+join({name:"saurabh" ,age:14 ,address:"hayatganj"});
